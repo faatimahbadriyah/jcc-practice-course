0eba970b323822ab84e0eb0c5c219e5c68e2c44d
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2021-12-15 17:02:01 +0700

    slack notification

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,28 +11,33 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $user = User::where('email', $request['email'])->first();
-        $data = [
-            'message' => 'login berhasil, session tersimpan',
-            'data' => [],
-        ];
-        if (is_null($user)) {
-            $data['message'] = 'login gagal: user tidak ditemukan';
-            Log::error($data['message'], ['data' => $request['email']]);
+        try {
+            $user = User::where('email', $request['email'])->first();
+            $data = [
+                'message' => 'login berhasil, session tersimpan',
+                'data' => [],
+            ];
+            if (is_null($user)) {
+                $data['message'] = 'login gagal: user tidak ditemukan';
+                Log::error($data['message'], ['data' => $request['email']]);
+                return response()->json($data, 400);
+            }
+
+            if (!Hash::check($request['password'], $user->password)) {
+                $data['message'] = 'login gagal: password salah';
+                Log::error($data['message'], ['data' => $request['email']]);
+                return response()->json($data, 400);
+            }
+
+            auth()->login($user);
+
+            $data['data'] = auth()->user();
+            Log::info($data['message'], $user->toArray());
+            return response()->json($data, 200);
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage(), []);
             return response()->json($data, 400);
         }
-
-        if (!Hash::check($request['password'], $user->password)) {
-            $data['message'] = 'login gagal: password salah';
-            Log::error($data['message'], ['data' => $request['email']]);
-            return response()->json($data, 400);
-        }
-
-        auth()->login($user);
-
-        $data['data'] = auth()->user();
-        Log::error($data['message'], $data['data']);
-        return response()->json($data, 200);
     }
 
     public function logout()
